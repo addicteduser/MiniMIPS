95b5d7c7266b457bce7edd03fbceeb0954e30d2a
TinTin Kalaw <tintin.kalaw@gmail.com>
2015-11-18 21:39:09 +0800

    Fixed comments.

--- MiniMIPS/src/dataObjects/Instruction.java
+++ MiniMIPS/src/dataObjects/Instruction.java
@@ -13,10 +13,9 @@ public class Instruction {
 	//private INSTRUCTIONTYPES instructionType;
 	private INSTRUCTIONS instructionName;
 	private String label;
-	private String rd;
-	private String rs;
-	private String rt;
-	private String imm;
+	private String v1;
+	private String v2;
+	private String v3;
 	
 	/**
 	 * Creates the mappings of the instructions to their Opcodes as well as the instruction type to its format.
@@ -34,7 +33,7 @@ public class Instruction {
 		
 		// R-type (Shift)
 		instructionFormat.put(INSTRUCTIONTYPES.RS, "0,0,RS,RD,SHF,iCODE");
-		instructionCode.put(INSTRUCTIONS.DSLL, "RS,56"); // DSLL RD,RS,SHF/IMM
+		instructionCode.put(INSTRUCTIONS.DSLL, "RS,56"); // DSLL RD,RS,SHF(IMM)
 		
 		// Extended R-type Instructions
 		instructionFormat.put(INSTRUCTIONTYPES.ER, "17,16,RT,RS,RD,iCODE");
@@ -44,145 +43,16 @@ public class Instruction {
 		// I-type Instructions
 		instructionFormat.put(INSTRUCTIONTYPES.I, "iCODE,RS,RT,IMM");
 		instructionCode.put(INSTRUCTIONS.BEQ, "I,4"); // BEQ RS,RT,IMM
-		instructionCode.put(INSTRUCTIONS.LW, "I,35"); // LW RT, IMM(RS)
-		instructionCode.put(INSTRUCTIONS.LWU, "I,39"); // LWU RT, IMM(RS)
-		instructionCode.put(INSTRUCTIONS.SW, "I,43"); // SW RT, IMM(RS)
+		instructionCode.put(INSTRUCTIONS.LW, "I,35"); // LW RT,IMM(RS)
+		instructionCode.put(INSTRUCTIONS.LWU, "I,39"); // LWU RT,IMM(RS)
+		instructionCode.put(INSTRUCTIONS.SW, "I,43"); // SW RT,IMM(RS)
 		instructionCode.put(INSTRUCTIONS.ANDI, "I,12"); // ANDI RT,RS,IMM
 		instructionCode.put(INSTRUCTIONS.DADDIU, "I,25"); // DADDIU RT,RS,IMM
-		instructionCode.put(INSTRUCTIONS.LS, "I,49"); // L.S RT, IMM(RS)
-		instructionCode.put(INSTRUCTIONS.SS, "I,57"); // S.S RT, IMM(RS)
+		instructionCode.put(INSTRUCTIONS.LS, "I,49"); // L.S RT,IMM(RS)
+		instructionCode.put(INSTRUCTIONS.SS, "I,57"); // S.S RT,IMM(RS)
 		
 		// J-type Instructions
 		instructionFormat.put(INSTRUCTIONTYPES.J, "iCODE,IMM");
 		instructionCode.put(INSTRUCTIONS.J, "J,2"); // J Label
 	}
-
-	/**
-	 * @return the instructionList
-	 */
-	public static ArrayList<Instruction> getInstructionList() {
-		return instructionList;
-	}
-
-	/**
-	 * @param instructionList the instructionList to set
-	 */
-	public static void setInstructionList(ArrayList<Instruction> instructionList) {
-		Instruction.instructionList = instructionList;
-	}
-
-	/**
-	 * @return the instructionCode
-	 */
-	public static EnumMap<INSTRUCTIONS, String> getInstructionCode() {
-		return instructionCode;
-	}
-
-	/**
-	 * @param instructionCode the instructionCode to set
-	 */
-	public static void setInstructionCode(EnumMap<INSTRUCTIONS, String> instructionCode) {
-		Instruction.instructionCode = instructionCode;
-	}
-
-	/**
-	 * @return the instructionFormat
-	 */
-	public static EnumMap<INSTRUCTIONTYPES, String> getInstructionFormat() {
-		return instructionFormat;
-	}
-
-	/**
-	 * @param instructionFormat the instructionFormat to set
-	 */
-	public static void setInstructionFormat(EnumMap<INSTRUCTIONTYPES, String> instructionFormat) {
-		Instruction.instructionFormat = instructionFormat;
-	}
-
-	/**
-	 * @return the instructionName
-	 */
-	public INSTRUCTIONS getInstructionName() {
-		return instructionName;
-	}
-
-	/**
-	 * @param instructionName the instructionName to set
-	 */
-	public void setInstructionName(INSTRUCTIONS instructionName) {
-		this.instructionName = instructionName;
-	}
-
-	/**
-	 * @return the label
-	 */
-	public String getLabel() {
-		return label;
-	}
-
-	/**
-	 * @param label the label to set
-	 */
-	public void setLabel(String label) {
-		this.label = label;
-	}
-
-	/**
-	 * @return the rd
-	 */
-	public String getRd() {
-		return rd;
-	}
-
-	/**
-	 * @param rd the rd to set
-	 */
-	public void setRd(String rd) {
-		this.rd = rd;
-	}
-
-	/**
-	 * @return the rs
-	 */
-	public String getRs() {
-		return rs;
-	}
-
-	/**
-	 * @param rs the rs to set
-	 */
-	public void setRs(String rs) {
-		this.rs = rs;
-	}
-
-	/**
-	 * @return the rt
-	 */
-	public String getRt() {
-		return rt;
-	}
-
-	/**
-	 * @param rt the rt to set
-	 */
-	public void setRt(String rt) {
-		this.rt = rt;
-	}
-
-	/**
-	 * @return the imm
-	 */
-	public String getImm() {
-		return imm;
-	}
-
-	/**
-	 * @param imm the imm to set
-	 */
-	public void setImm(String imm) {
-		this.imm = imm;
-	}
-
-	
-	
 }
