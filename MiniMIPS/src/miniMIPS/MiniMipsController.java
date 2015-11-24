42411bdb5411220cb3c1d4d9abc40d92e7531193
TinTin Kalaw <tintin.kalaw@gmail.com>
2015-11-25 01:14:19 +0800

    Modified UI functionality.

--- MiniMIPS/src/miniMIPS/MiniMipsController.java
+++ MiniMIPS/src/miniMIPS/MiniMipsController.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JFileChooser;
 
+import constants.INSTRUCTIONS;
 import dataObjects.Data;
 import dataObjects.Instruction;
 import parser.FileParser;
@@ -12,17 +13,18 @@ import parser.FileParser;
 public class MiniMipsController {
 	private MiniMipsUI frame;
 	private FileParser fileparser;
-	
+
 	public MiniMipsController() {
 		frame = new MiniMipsUI();
 		frame.addBtnLoadMipsCodeActionListener(new LoadMipsCodeActionLister());
+		frame.addCbInstructionActionListener(new CbInstructionActionListener());
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
-	
+
 	private class LoadMipsCodeActionLister implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			MiniMipsUI.resetTblModCode();
+			frame.resetTblModCode();
 			Instruction.resetInstructionList();
 			Data.resetDataList();
 			JFileChooser fileChooser = new JFileChooser(".\\_test");
@@ -33,6 +35,28 @@ public class MiniMipsController {
 				GuiUpdater.loadCodeTable();
 			}
 		}
-		
+
+	}
+
+	private class CbInstructionActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			INSTRUCTIONS instruction = INSTRUCTIONS.valueOf(frame.getCbInstructionSelected());
+
+			switch(instruction) {
+			case DADDU: case OR: case SLT: // RD,RS,RT
+			case ADDS: case MULS: // FD,FS,FT
+				frame.inputRType();
+			case DSLL: // RD,RS,SHF/IMM
+			case ANDI: case DADDIU: // RS,RT,IMM
+			case LW: case LWU: case SW: // RT, IMM(RS)
+			case LS: case SS: // FT, IMM(RS)
+				frame.inputIType();
+				break;
+			case DMULT: // RS,RT
+				break;
+			case BEQ: case J: // Label
+				break;
+			}
+		}
 	}
 }
