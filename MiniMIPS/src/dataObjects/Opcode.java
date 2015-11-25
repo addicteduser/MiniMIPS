86345d0bdaabc41bd38c654a2f1b57517947f37b
TinTin Kalaw <tintin.kalaw@gmail.com>
2015-11-25 14:17:42 +0800

    Successfully generated opcode for R-Type instruction case.

--- MiniMIPS/src/dataObjects/Opcode.java
+++ MiniMIPS/src/dataObjects/Opcode.java
@@ -6,7 +6,8 @@ import constants.INSTRUCTIONS;
 import constants.INSTRUCTIONTYPES;
 
 public class Opcode {
-	private String opcodeHex;
+	private static String opcodeHex;
+	private static String opcodeBin;
 	private static String IR0_5; // opcode
 	private static String IR6_10; // a
 	private static String IR11_15; // b
@@ -23,16 +24,29 @@ public class Opcode {
 		//iType = i.getInstructionName();
 		parseICode(Instruction.getInstructionCode().get(iName));
 		parseIFormat(Instruction.getInstructionFormat().get(iType));
-		buildOpcode();
+		buildOpcode(i);
 		
 		return temp;
 	}
 	
-	private static void buildOpcode() {
+	private static void buildOpcode(Instruction i) {
+		int tempVal;
+		String imm = "";
 		switch(iType) {
-		case R:
-			IR0_5 = String.format("%6s", Integer.toBinaryString(iCode & 0xFF)).replace(' ', '0');
-			System.out.println(IR0_5);
+		case R: // 0,RS,RT,RD,0,iCODE
+			tempVal = Integer.parseInt(formatTokens[0]);
+			IR0_5 = String.format("%6s", Integer.toBinaryString(tempVal & 0xFF)).replace(' ', '0');
+			tempVal = Integer.parseInt(i.getV2().substring(1));
+			IR6_10 = String.format("%5s", Integer.toBinaryString(tempVal & 0xFF)).replace(' ', '0');
+			tempVal = Integer.parseInt(i.getV3().substring(1));
+			IR11_15 = String.format("%5s", Integer.toBinaryString(tempVal & 0xFF)).replace(' ', '0');
+			tempVal = Integer.parseInt(i.getV1().substring(1));
+			imm = String.format("%5s", Integer.toBinaryString(tempVal & 0xFF)).replace(' ', '0');
+			tempVal = Integer.parseInt(formatTokens[4]);
+			imm = imm.concat(String.format("%5s", Integer.toBinaryString(tempVal & 0xFF)).replace(' ', '0'));
+			tempVal = iCode;
+			imm = imm.concat(String.format("%5s", Integer.toBinaryString(tempVal & 0xFF)).replace(' ', '0'));
+			IR16_31 = imm;
 			break;
 		case ER:
 			break;
@@ -46,6 +60,8 @@ public class Opcode {
 			break;
 		
 		}
+		opcodeBin = IR0_5+IR6_10+IR11_15+IR16_31;
+		opcodeHex = String.format("%08X", Long.parseLong(opcodeBin, 2));
 	}
 	
 	private static void parseICode(String code) {
